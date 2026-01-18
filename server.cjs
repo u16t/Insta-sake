6dd47fb93dc1e243778da97c674394bfbfc52d0d
Yuichiro Tanaka <u16.tanaka@gmail.com>
2026-01-18 12:35:14 +0900

    use regex catch-all route for express v5

--- server.cjs
+++ server.cjs
@@ -543,7 +543,7 @@ nodeCron.schedule('* * * * *', async () => {
 const distPath = path.join(__dirname, 'dist');
 if (fs.existsSync(distPath)) {
     app.use(express.static(distPath));
-    app.get('/*', (req, res) => {
+    app.get(/.*/, (req, res) => {
         res.sendFile(path.join(distPath, 'index.html'));
     });
 }
